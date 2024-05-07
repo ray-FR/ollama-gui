f722f45233bdc4d68281efec435cc3489638ac51
Ray-FR <ares1345istaken@gmail.com>
2024-05-07 22:19:34 +0200

    testing

--- gui.py
+++ gui.py
@@ -1,8 +1,20 @@
 import ollama
+import tkinter
+import customtkinter as ctk
 
 
 
 
+app = ctk.CTk()
+app.geometry('600x400')
+app.title('test')
+app.resizable(True, False)
+
+
+test = ctk.CTkLabel(app, width=300, text="AAA")
+test.pack()
+entry = ctk.CTkEntry(app, width=200, placeholder_text="testing")
+entry.pack(padx = 20, pady = 50)
 
 
 
@@ -13,7 +25,7 @@ a = (ollama.list())
 print(a['models'][0]['name'])
 print(a['models'][1]['name'])
 print(len(a['models']))
-"""
+
 response = ollama.chat(model='phi3', messages=[
   {
     'role': 'user',
@@ -21,3 +33,6 @@ response = ollama.chat(model='phi3', messages=[
   },
 ])
 print(response['message']['content'])
+"""
+
+app.mainloop()

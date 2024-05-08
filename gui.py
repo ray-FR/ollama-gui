8c4c14e46b0e7a75f202e588f206754e95ce2199
Ray-FR <ares1345istaken@gmail.com>
2024-05-08 13:49:43 +0200

    advancing

--- gui.py
+++ gui.py
@@ -7,6 +7,15 @@ import importlib
 
 
 
+def testy(app):
+    
+    response = ollama.chat(model='phi3', messages=[
+    {
+    'role': 'user',
+    'content': txt.get(),
+    },
+    ])
+    test.configure(text=response['message']['content'])
 
 
 
@@ -14,12 +23,17 @@ app = ctk.CTk()
 app.geometry('600x400')
 app.title('test')
 app.resizable(True, False)
+txt = ctk.StringVar()
 
-
-test = ctk.CTkLabel(app, width=300, text="AAA")
-test.pack()
-entry = ctk.CTkEntry(app, width=200, placeholder_text="testing")
+test = ctk.CTkLabel(app, width=450, text='a', font=('Arial', 15), fg_color='gray')
+test.pack(pady=20)
+entry = ctk.CTkEntry(app, width=500, placeholder_text="write something", textvariable=txt)
 entry.pack(padx = 20, pady = 50)
+
+entry.bind('<Return>', testy)
+
+
+
 
 
 
